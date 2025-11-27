b0e8822571bf6afabcdd709b9a83f7f00c983810
LeisurelyLuke <l.korzanowski@gmail.com>
2025-11-27 18:56:37 +0100

    Modified main.py. Now it reads a filepath and counts words.

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(book_path):
+    with open(book_path) as book:
+        return book.read()
+
+def main():
+    book_path = "books/frankenstein.txt"
+    book_text = get_book_text(book_path)
+    num_words = len(book_text.split())
+    print(f"Found {num_words} total words")
+
+main()  
