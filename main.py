b13cbf72786d84a400593349ec55448000529e62
LeisurelyLuke <l.korzanowski@gmail.com>
2025-11-29 13:58:12 +0100

    unique character counting function

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from stats import get_num_words #importing word-counting function
+from stats import get_unique_character_count #importing unique character counting function
 
 def main():
     get_num_words() #calls word-counting function
+    get_unique_character_count() #calls unique character counting function
 
 main()
--- stats.py
+++ stats.py
@@ -7,3 +7,15 @@ def get_num_words():
     book_text = get_book_text(book_path)
     num_words = len(book_text.split())
     print(f"Found {num_words} total words")
+
+def get_unique_character_count():
+    unique_character_dict = {}
+    book_path = "books/frankenstein.txt"
+    book_text = get_book_text(book_path)
+    for character in book_text:
+        lower_character = character.lower()
+        if lower_character in unique_character_dict:
+            unique_character_dict[lower_character] += 1
+        else:
+            unique_character_dict[lower_character] = 1
+    print(unique_character_dict)
